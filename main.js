c92a07c42bfccf002cf5193777f79d31ba85edee
AdamDCosta <adam.dcosta@googlemail.com>
2021-09-24 13:03:43 +0100

    game class started - create grid function added

--- main.js
+++ main.js
@@ -6,18 +6,34 @@ const startPage = document.querySelector("header")
 
 const gameArea = document.querySelector(".game__area")
 
+const gridHTML = document.querySelectorAll(".game__area--cell")
 
 
+class Game {
+  constructor(cells) {
+    this.cells = cells;
+    this.isPlayerOnesTurn = true;
+    this.isPlayerTwosTurn = false;
+  }
 
-const startGame = () => {
-  startBtn.addEventListener("click", () => {
-    console.log(startPage)
-    startPage.classList.remove("intro")
-    startPage.classList.add("hide-intro")
+  startGame() {
+    startBtn.addEventListener("click", () => {
+      console.log(startPage)
+      startPage.classList.remove("intro")
+      startPage.classList.add("hide-intro")
+      console.log("hello")
+    })
+  }
 
-    console.log("hello")
-    
-  })
+  createGrid(){ 
+    for ( let i = 0; i < 42; i++ ) { 
+    let cell = document.createElement("div") 
+    gameArea.appendChild(cell).className = "game__area--cell game__area--cell" + i;
+    }
+
 }
+}
+const game = new Game(gridHTML)
 
-startGame();
+game.startGame();
+game.createGrid();
