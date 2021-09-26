c947cab354fb477543ee6ec01cb3423bbff7c65a
AdamDCosta <adam.dcosta@googlemail.com>
2021-09-26 15:05:17 +0100

    dynamic grid replaced with HTML

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const gameArea = document.querySelector(".game__area");
 
 const gridHTML = document.querySelectorAll(".game__area--cell");
 
+const gridArray = Array.from(gridHTML)
+console.log(gridArray[48])
+
 const restart = document.querySelector(".game__reset");
 
 const gamePlayer = document.querySelector(".game__player");
@@ -25,36 +28,35 @@ class Game {
       startPage.classList.remove("intro")
       startPage.classList.add("hide-intro")
       gamePlayer.innerHTML = "Player One"
-      this.createArray();
-      
+      // game.createGrid(7, 7);
       
     })
   }
 
-  createGrid(rows, cols){ 
-    gameArea.style.setProperty("grid-template-rows", "repeat(" + rows + ", 1fr)");
-    gameArea.style.setProperty("grid-template-columns", "repeat(" + cols + ", 1fr)");
-    for ( let i = 0; i < 42; i++ ) { 
-      let cell = document.createElement("div") 
-      gameArea.appendChild(cell).className = "game__area--cell game__area--cell" + i;
-    }
-    for ( let i = 0; i >= 42; i++ ) {
-      let cell = document.createElement("div") 
-      gameArea.appendChild(cell).className = "game__area--taken game__area--taken" + i;
-    }
-  }
+  // createGrid(rows, cols){ 
+  //   gameArea.style.setProperty("grid-template-rows", "repeat(" + rows + ", 1fr)");
+  //   gameArea.style.setProperty("grid-template-columns", "repeat(" + cols + ", 1fr)");
+  //   for ( let i = 0; i < 42; i++ ) { 
+  //     let cell = document.createElement("div") 
+  //     gameArea.appendChild(cell).className = "game__area--cell game__area--cell" + i;
+  //   }
+  //   for ( let i = 0; i >= 42; i++ ) {
+  //     let cell = document.createElement("div") 
+  //     gameArea.appendChild(cell).className = "game__area--taken game__area--taken" + i;
+  //   }
+  // }
 
-  createArray() {
-    const gridArray = Array.from(gridHTML)
-    console.log(gridArray)
-  }
+  // createArray(cells) {
+  //   const gridArray = Array.from(this.cells)
+  //   console.log(gridArray)
+  // }
 
-  firstTurn() {
+  playerTurn() {
     this.cells.forEach(gridCell => {
       gridCell.addEventListener("click", () => {
         
         console.log("boom")
-        this.handlePlayersTurn(cell)
+        this.handlePlayersTurn(gridCell)
         // this.checkWin();
         })
     })
@@ -86,6 +88,6 @@ const game = new Game(gridHTML)
 
 
 game.startGame();
-game.createGrid(7, 7);
-game.firstTurn()
+
+game.playerTurn()
 
