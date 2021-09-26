a4d25d319241685da782568cc5e0e5047a679150
AdamDCosta <adam.dcosta@googlemail.com>
2021-09-26 15:04:55 +0100

    player turn function added

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // --> DOM ELEMENTS
 
-const startBtn = document.querySelector(".intro__button")
+const startBtn = document.querySelector(".intro__button");
 
-const startPage = document.querySelector("header")
+const startPage = document.querySelector("header");
 
-const gameArea = document.querySelector(".game__area")
+const gameArea = document.querySelector(".game__area");
 
-const gridHTML = document.querySelectorAll(".game__area--cell")
+const gridHTML = document.querySelectorAll(".game__area--cell");
 
+const restart = document.querySelector(".game__reset");
+
+const gamePlayer = document.querySelector(".game__player");
 
 class Game {
   constructor(cells) {
@@ -21,7 +24,10 @@ class Game {
       console.log(startPage)
       startPage.classList.remove("intro")
       startPage.classList.add("hide-intro")
-      console.log("hello")
+      gamePlayer.innerHTML = "Player One"
+      this.createArray();
+      
+      
     })
   }
 
@@ -29,22 +35,57 @@ class Game {
     gameArea.style.setProperty("grid-template-rows", "repeat(" + rows + ", 1fr)");
     gameArea.style.setProperty("grid-template-columns", "repeat(" + cols + ", 1fr)");
     for ( let i = 0; i < 42; i++ ) { 
-    let cell = document.createElement("div") 
-    gameArea.appendChild(cell).className = "game__area--cell game__area--cell" + i;
+      let cell = document.createElement("div") 
+      gameArea.appendChild(cell).className = "game__area--cell game__area--cell" + i;
     }
-}
+    for ( let i = 0; i >= 42; i++ ) {
+      let cell = document.createElement("div") 
+      gameArea.appendChild(cell).className = "game__area--taken game__area--taken" + i;
+    }
+  }
+
+  createArray() {
+    const gridArray = Array.from(gridHTML)
+    console.log(gridArray)
+  }
 
   firstTurn() {
-    this.cells.forEach(cell => {
-      square.addEventListener("click", () => {
+    this.cells.forEach(gridCell => {
+      gridCell.addEventListener("click", () => {
+        
+        console.log("boom")
         this.handlePlayersTurn(cell)
-        this.checkWin();
+        // this.checkWin();
         })
     })
   }
 
+  handlePlayersTurn(clickedCell) {
+    if (clickedCell.textContent) {
+      return;
+    }
+    
+    if (this.isPlayerOnesTurn )  {
+      clickedCell.textContent = "X";
+      this.isPlayerOnesTurn = false;
+      this.isPlayerTwosTurn = true;
+    }
+    else {
+        clickedCell.textContent = "O";
+        this.isPlayerOnesTurn = true;
+        this.isPlayerTwosTurn = false;
+      
+    }
+  }
+
+  
 }
+
 const game = new Game(gridHTML)
 
+
+
 game.startGame();
-game.createGrid(7, 6);
+game.createGrid(7, 7);
+game.firstTurn()
+
